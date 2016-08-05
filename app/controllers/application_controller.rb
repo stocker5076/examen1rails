da79bdb81f60c4156973bdd921fe579cc8b7f709
stocker5076 <stocker5076@gmail.com>
2016-08-04 20:50:00 -0500

    Agregando a superman

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :set_layout
+
+  def set_layout
+  	if controller_name == "pages" and action_name == "superman"
+  		return 'supermanlayout'
+  	else
+  		return  'application'
+  	end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def batman
   end
+
+  def superman
+  end
 end
